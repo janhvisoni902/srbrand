b24904f15e875d452221c0d6e7578de7f5894ae8
janhvisoni902 <janhvisoni902@gmail.com>
2026-09-08 22:51:57 +0530

    Keep only the backend folder, remove everything else

--- src/lib/gsap.js
+++
@@ -1,9 +0,0 @@
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { useGSAP } from '@gsap/react';
-
-// Register plugins once
-gsap.registerPlugin(ScrollTrigger, useGSAP);
-
-export { gsap, ScrollTrigger };
-export default gsap;
